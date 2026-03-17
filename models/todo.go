356054d5d93e08464a9abcbfa9da710c3bfb81ed
Eetu <99178022+E3TU@users.noreply.github.com>
2026-03-17 22:43:52 +0200

    Todo model

---
+++ models/todo.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type Todo struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Desc      string    `json:"desc"`
+	Completed bool      `json:"completed"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
