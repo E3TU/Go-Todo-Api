63b26b316a1845a0aa32d8778337773c992627d0
Eetu <99178022+E3TU@users.noreply.github.com>
2026-03-22 19:10:15 +0200

    Simplified todo struct

--- models/todo.go
+++ models/todo.go
@@ -1,12 +1,8 @@
 package models
 
-import "time"
-
 type Todo struct {
-	ID        int       `json:"id"`
-	Title     string    `json:"title"`
-	Desc      string    `json:"desc"`
-	Completed bool      `json:"completed"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        string `json:"id"`
+	Title     string `json:"title"`
+	Desc      string `json:"desc"`
+	Completed bool   `json:"completed"`
 }
